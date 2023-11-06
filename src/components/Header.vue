c2e0858d7117dd042c7ef65a2d60704361301846
DamianoL <damianolunardo@hotmail.it>
2023-11-06 15:59:43 +0100

    ADD header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,20 +2,66 @@
 export default {
   data() {
     return {
-      title: 'Header'
-    }
-  }
-}
+      title: "Header",
+    };
+  },
+};
 </script>
 
 <template>
-<header>
-   
-</header>
+  <header>
+    <div class="row container">
+      <div class="col-4">
+        <ul>
+          <li>Donna</li>
+          <li>Uomo</li>
+          <li>Bambino</li>
+        </ul>
+      </div>
+      <div class="col-4">
+        <img class="logo1" src="/img/boolean-logo.png" />
+      </div>
+      <div class="col-4">
+        <img class="person" src="/img/person-line-drawing-svgrepo-com.svg" />
+        <img class="heart" src="/img/heart-svgrepo-com.svg" />
+        <img clas="bag" src="/img/bag-svgrepo-com.svg" />
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+ul,
+li {
+  display: flex;
+  gap: 10px;
+}
+
+header {
+  padding-top: 20px;
+  background-color: $header-color;
+  font-weight: bold;
+  font-size: 20px;
+  color: white;
+}
+
+.logo1 {
+  align-items: center;
+  height: 50px;
+  width: 210px;
+}
+
+.heart {
+  width: 30px;
+  height: 30px;
+}
+
+.person {
+  width: 30px;
+  height: 30px;
+}
+
 
 
 </style>
-
