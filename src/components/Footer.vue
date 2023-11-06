50d10f68f08386ca9a798d2e71a4953412cb4f2d
DamianoL <damianolunardo@hotmail.it>
2023-11-06 18:05:51 +0100

    FIX footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -16,37 +16,39 @@ export default {
           Boolando s.r.l <br />
           Informazioni &nbsp&nbsp Legali Informativa sulla privacy &nbsp&nbsp
           Diritto di recesso
-        </div>
-        <div class="col-4">
-          Trovaci anche su
           <br />
-          <a href="https://twitter.com/home" target="_blank">
-            <img
-              class="logo"
-              src="/img/5296514_bird_tweet_twitter_twitter logo_icon.svg"
-            />
-          </a>
-          <a href="https://www.instagram.com" target="_blank">
-            <img
-              class="logo"
-              src="/img/5296765_camera_instagram_instagram logo_icon.svg"
-            />
-          </a>
-          <a href="https://www.facebook.com" target="_blank">
-            <img
-              class="logo"
-              src="/img/5296499_fb_facebook_facebook logo_icon.svg"
-            />
-          </a>
-          <a href="https://www.youtube.com" target="_blank">
-            <img
-              class="logo"
-              src="/img/5296521_play_video_vlog_youtube_youtube logo_icon.svg"
-            />
-          </a>
-          <a href="https://www.pinterest.it" target="_blank">
-            <img class="logo" src="/img/pinterest-logo.svg" />
-          </a>
+        </div>
+        <div class="col-4 prova">
+          <div>Trovaci anche su</div>
+          <div>
+            <a href="https://twitter.com/home" target="_blank">
+              <img
+                class="logo"
+                src="/img/5296514_bird_tweet_twitter_twitter logo_icon.svg"
+              />
+            </a>
+            <a href="https://www.instagram.com" target="_blank">
+              <img
+                class="logo"
+                src="/img/5296765_camera_instagram_instagram logo_icon.svg"
+              />
+            </a>
+            <a href="https://www.facebook.com" target="_blank">
+              <img
+                class="logo"
+                src="/img/5296499_fb_facebook_facebook logo_icon.svg"
+              />
+            </a>
+            <a href="https://www.youtube.com" target="_blank">
+              <img
+                class="logo"
+                src="/img/5296521_play_video_vlog_youtube_youtube logo_icon.svg"
+              />
+            </a>
+            <a href="https://www.pinterest.it" target="_blank">
+              <img class="logo" src="/img/pinterest-logo.svg" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
@@ -56,8 +58,8 @@ export default {
 <style lang="scss" scoped>
 @use "../styles/partials/variables" as *;
 .footer {
-    background-color: $footer-color;
-    color: $text_color-white;
+  background-color: $footer-color;
+  color: $text_color-white;
 }
 .rowf {
   padding-top: 10px;
@@ -70,5 +72,12 @@ export default {
   padding-top: 5px;
   width: 20px;
   height: 20px;
+}
+
+.prova {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
 }
 </style>
